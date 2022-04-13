060de283294c0a1c81adf74cf1d42f56583da973
Pythonmoders <100186554+Pythonmoders@users.noreply.github.com>
2022-04-13 16:09:27 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/surabhichinnu1/Professor-Bot.git /Professor-Bot
+  git clone https://github.com/surabhichinnu1/Professor-Bot.git /Ajax
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Professor-Bot
+  git clone $UPSTREAM_REPO /Ajax
 fi
 cd /Ajax
 pip3 install -U -r requirements.txt
